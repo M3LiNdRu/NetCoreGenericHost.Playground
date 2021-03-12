31b8d3efa350503b109f30d0a10611eb9f1e17a7
Roger Calaf Planell <rogercalaf@gmail.com>
2021-03-12 21:05:20 +0100

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //overiding Microsoft.Extensions.Hosting.Internal.ConsoleLifetime
+                    services.AddSingleton<IHostLifetime, ExampleHostLifetime>();
+
+                    services.AddHostedService<HostedService>();
+                });
+    }
+}

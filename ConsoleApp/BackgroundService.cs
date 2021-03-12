46d673a5d6403e44cf9e58919c530dd048981c54
Roger Calaf Planell <rogercalaf@gmail.com>
2021-03-12 21:20:32 +0100

    shutting down background service after execution

--- ConsoleApp/BackgroundService.cs
+++ ConsoleApp/BackgroundService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,17 +11,24 @@ namespace ConsoleApp
         private readonly Guid _id;
 
         private readonly ILogger<BackgroundService> _logger;
+        private readonly IHostApplicationLifetime _applicationLifetime;
 
-        public BackgroundService(ILogger<BackgroundService> logger)
+        public BackgroundService(ILogger<BackgroundService> logger, IHostApplicationLifetime applicationLifetime)
         {
             _id = Guid.NewGuid();
             _logger = logger;
+            _applicationLifetime = applicationLifetime;
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _logger.LogInformation("Executing background task in background service id {id}", _id);
-            return Task.Delay(10000);
+
+            await Task.Delay(10000);
+
+            _logger.LogInformation("Finished background task in background service id {id}", _id);
+
+            _applicationLifetime.StopApplication();
         }
     }
 }
